8d32d8e46481a8ae32fab9c01a49190516a89c6d
kutidake <kutidake@icloud.com>
2021-12-30 18:00:26 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,12 @@ class User < ApplicationRecord
         #  has_many :orders
 
          validates :nickname, presence: true
-         with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+         with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヶ\p{Ideographic}]+\z/, message: 'に全角文字を使用してください' } do
           validates :last_name
           validates :first_name
         end
          validates :first_name_f, :last_name_f,format:{with:/\A[ァ-ヶー－]+\z/,message:"カタカナでご入力下さい。"}
-         validates :last_name, presence: true
          validates :last_name_f, presence: true
-         validates :first_name, presence: true
          validates :first_name_f, presence: true
          validates :birth, presence: true
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
