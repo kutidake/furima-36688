6d7633ffcaedc0fc822dddd54503706370975216
kutidake <kutidake@icloud.com>
2022-01-17 20:43:57 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit,:update]
   def index
     @items = Item.all.order("created_at DESC")
     # @item = Item.find(params[:item_id])
@@ -6,9 +9,6 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
   
 
@@ -26,19 +26,18 @@ class ItemsController < ApplicationController
   #   @item.destroy
   # end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  # end
+  def edit
+  end
 
-  # def update
-  #   @item = Item.find(params[:id])
-  #   @item.update(item_params)
-  #   if @item.save
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   def show
     @item = Item.find(params[:id])
@@ -49,4 +48,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image,:item_name, :explanation, :category_id, :condition_id, :charge_id, :area_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+     if item.user_id != current_user.id
+     redirect_to action: :index
+     end
+     end
 end
